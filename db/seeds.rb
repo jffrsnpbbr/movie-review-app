6c6bbc664811028c7a69ab66f7740295b08eec03
jffrsnpbbr <93456420+jffrsnpbbr@users.noreply.github.com>
2023-04-19 11:26:21 +0800

    add genre seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Genre.create([{ name: 'action' },
+              { name: 'animation' },
+              { name: 'comedy' },
+              { name: 'drama' },
+              { name: 'experimental' },
+              { name: 'fantasy' },
+              { name: 'historical' },
+              { name: 'horror' },
+              { name: 'romance' },
+              { name: 'science fiction' },
+              { name: 'thriller' },
+              { name: 'western' }])
